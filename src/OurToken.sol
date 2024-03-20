313090f9ca775c6a2e1bfb58aba542761f31ae26
jayalwayscodes <johnsonjardocs@gmail.com>
2024-03-20 20:19:26 +0100

    installed openzeppelin

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -1,3 +1,11 @@
 //SPDX-License_Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
