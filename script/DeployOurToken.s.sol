41977af00b39aa843235db56f9788cbe5350b77e
jayalwayscodes <johnsonjardocs@gmail.com>
2024-03-21 01:02:41 +0100

    last commit

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -5,11 +5,13 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
 
+
 contract DeployOurToken is Script{
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
-    function run() external {
+    function run() external returns (OurToken) {
         vm.startBroadcast();
-        new OurToken(INITIAL_SUPPLY);
+        OurToken ot = new OurToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
+        return ot;
     }
 } 
